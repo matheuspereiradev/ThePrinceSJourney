40fe45ba5d429712a3ec83376ef0cebb3acf0acf
Matheus <Matheus@MATHEUS-LIMA>
2019-07-14 19:41:19 -0300

    Mudança de pastas para padronização das musicas

--- src/com/matheus/game/Sons.java
+++ src/com/matheus/game/Sons.java
@@ -7,14 +7,14 @@ import java.applet.AudioClip;
 public class Sons {
 
 	private AudioClip song;
-	public static final Sons musica=new Sons("/Windless Slopes.wav");
-	public static final Sons danoSong=new Sons("/hurt.wav");
-	public static final Sons danoInimigoSong=new Sons("/dano_inimigo.wav");
-	public static final Sons bipMenu=new Sons("/bip.wav");
-	public static final Sons vidaSong=new Sons("/vida.wav");
-	public static final Sons tiroSong=new Sons("/tiro.wav");
-	public static final Sons naoPodeSong=new Sons("/nao_pode.wav");
-	public static final Sons proxFase=new Sons("/nextLevel.wav");
+	public static final Sons musica=new Sons("/music/Windless Slopes.wav");
+	public static final Sons danoSong=new Sons("/music/hurt.wav");
+	public static final Sons danoInimigoSong=new Sons("/music/dano_inimigo.wav");
+	public static final Sons bipMenu=new Sons("/music/bip.wav");
+	public static final Sons vidaSong=new Sons("/music/vida.wav");
+	public static final Sons tiroSong=new Sons("/music/tiro.wav");
+	public static final Sons naoPodeSong=new Sons("/music/nao_pode.wav");
+	public static final Sons proxFase=new Sons("/music/nextLevel.wav");
 	
 
 	public Sons(String path) {
