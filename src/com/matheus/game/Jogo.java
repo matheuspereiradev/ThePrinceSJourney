0e02facabf3b1bcfc8705df7b3b4115017713239
Matheus <Matheus@MATHEUS-LIMA>
2019-07-20 12:45:27 -0300

    Correção da passagem de fase ao salvar

--- src/com/matheus/game/Jogo.java
+++ src/com/matheus/game/Jogo.java
@@ -103,7 +103,7 @@ public class Jogo extends Canvas implements Runnable, KeyListener {
 			if(this.saveGame) {
 				this.saveGame=false;
 				String [] opt1= {"level"};
-				int []opt2= {this.fase};
+				int []opt2= {fase};
 				Salvar.salvarJogo(opt1, opt2, 19);
 				System.out.println("Salvo");
 			}
